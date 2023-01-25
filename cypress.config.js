4209701c4d450cf11568cfea188304ede26a6899
Dawid Dutkiewicz <dawid.dutkiewicz@gmail.com>
2023-01-25 13:49:48 +0100

    Update Cypress baseUrl and docs (#6)
    * Change domain to https://ga-demo.dhdev.co/
    
    * Update package.json details
    
    * Update README.md with new environment details
    
    Co-authored-by: David Dutkiewicz <dawid@dashhudson.com>

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://ga-ecomm-demo-test.netlify.app',
+    baseUrl: 'https://ga-demo.dhdev.co',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
